b8b7c3b03b66cc9f9d73c24d31a1a2a0f84c6c71
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2024-12-26 14:33:41 +0530

    Revert "Fixed the scrolled event parameters are incorrect issue"
    This reverts commit b9aa20d5d7d070027920f7b55e75f185b70a5efe.

--- src/Controls/src/Core/Handlers/Items/Android/RecyclerViewScrollListener.cs
+++ src/Controls/src/Core/Handlers/Items/Android/RecyclerViewScrollListener.cs
@@ -1,5 +1,4 @@
 ﻿#nullable disable
-using System;
 using AndroidX.RecyclerView.Widget;
 
 namespace Microsoft.Maui.Controls.Handlers.Items
@@ -88,52 +87,6 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 				lastVisibleItemIndex = linearLayoutManager.FindLastVisibleItemPosition();
 				centerItemIndex = recyclerView.CalculateCenterItemIndex(firstVisibleItemIndex, linearLayoutManager, _getCenteredItemOnXAndY);
 			}
-
-			var hasHeader = ItemsViewAdapter.ItemsSource.HasHeader;
-			var hasFooter = ItemsViewAdapter.ItemsSource.HasFooter;
-			var itemsCount = ItemsViewAdapter.ItemCount;
-
-			if (firstVisibleItemIndex == 0 && lastVisibleItemIndex == itemsCount - 1)
-			{
-				lastVisibleItemIndex -= 2;
-			}
-			else
-			{
-				if (hasHeader && !hasFooter)
-				{
-					lastVisibleItemIndex -= 1;
-					if (firstVisibleItemIndex != 0)
-					{
-						firstVisibleItemIndex -= 1;
-					}
-				}
-				else if (!hasHeader && hasFooter)
-				{
-					if (lastVisibleItemIndex == itemsCount - 1)
-					{
-						lastVisibleItemIndex -= 1;
-						firstVisibleItemIndex -= 1;
-					}
-				}
-				else if (hasHeader && hasFooter)
-				{
-					if (firstVisibleItemIndex == 0)
-					{
-						lastVisibleItemIndex -= 1;
-					}
-					else if (firstVisibleItemIndex != 0 && lastVisibleItemIndex != itemsCount - 1)
-					{
-						firstVisibleItemIndex -= 1;
-						lastVisibleItemIndex -= 1;
-					}
-					else
-					{
-						firstVisibleItemIndex -= 1;
-						lastVisibleItemIndex -= 2;
-					}
-				}
-			}
-
 			return (firstVisibleItemIndex, centerItemIndex, lastVisibleItemIndex);
 		}
 
