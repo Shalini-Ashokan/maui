2cb4409413094c848a5649c17e3554e0e27ffc97
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2024-12-30 15:21:36 +0530

    Fixed the shell content isvisible as false the app crash

--- src/Controls/src/Core/Handlers/Shell/Windows/ShellView.cs
+++ src/Controls/src/Core/Handlers/Shell/Windows/ShellView.cs
@@ -195,6 +195,11 @@ namespace Microsoft.Maui.Controls.Platform
 		{
 			var navItems = FlyoutItems.OfType<NavigationViewItemViewModel>();
 
+			if (newItem == null)
+			{
+				return;
+			}
+
 			// Implicit items aren't items that are surfaced to the user 
 			// or data structures. So, we just want to find the element
 			// the user defined on Shell
