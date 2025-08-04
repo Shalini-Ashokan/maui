1aa9b73174dff9e4b04dd137e65e233facf291c7
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2025-08-04 15:52:55 +0530

    correct the api failure

--- src/Core/src/Handlers/SearchBar/SearchBarHandler.Standard.cs
+++ src/Core/src/Handlers/SearchBar/SearchBarHandler.Standard.cs
@@ -36,6 +36,6 @@ namespace Microsoft.Maui.Handlers
 		public static void MapMaxLength(IViewHandler handler, ISearchBar searchBar) { }
 		public static void MapIsReadOnly(IViewHandler handler, ISearchBar searchBar) { }
 		public static void MapKeyboard(IViewHandler handler, ISearchBar searchBar) { }
-		public static void MapCursorPosition(IViewHandler handler, ISearchBar searchBar) { }
+		public static void MapCursorPosition(ISearchBarHandler handler, ISearchBar searchBar) { }
 	}
 }
