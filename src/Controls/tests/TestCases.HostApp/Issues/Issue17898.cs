4dce7d5ff236f04e76de83f3392d3b94ccfdfc34
Shalini-Ashokan <102292178+Shalini-Ashokan@users.noreply.github.com>
2026-03-03 14:31:53 +0530

    Removed the unwanted changes

--- src/Controls/tests/TestCases.HostApp/Issues/Issue17898.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue17898.cs
@@ -16,14 +16,14 @@ public class Issue17898 : TestShell
 		var normalState = new VisualState { Name = "Normal" };
 		normalState.Setters.Add(new Setter
 		{
-			Property = VisualElement.BackgroundColorProperty,
+			Property = VisualElement.BackgroundProperty,
 			Value = new AppThemeBinding { Light = normalLight, Dark = normalDark }
 		});
 
 		var selectedState = new VisualState { Name = "Selected" };
 		selectedState.Setters.Add(new Setter
 		{
-			Property = VisualElement.BackgroundColorProperty,
+			Property = VisualElement.BackgroundProperty,
 			Value = selectedRed
 		});
 
